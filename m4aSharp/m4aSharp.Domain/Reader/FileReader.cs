500cf829f4d2db13b4504c4d559eb7fb86837f4a
Rnady <randy.briest@gmail.com>
2020-07-02 08:05:12 -0500

    Added a neat little note for my dang self so I can come back later and write the parser.

--- m4aSharp/m4aSharp.Domain/Reader/FileReader.cs
+++ m4aSharp/m4aSharp.Domain/Reader/FileReader.cs
@@ -2,6 +2,9 @@
 
 namespace m4aSharp.Domain.Reader
 {
+    // TODO: Once we write the file parser, let's use these as sources:
+    // https://www.file-recovery.com/m4a-signature-format.htm
+    // https://ahyattdev.github.io/blog/2018/02/17/m4a-metadata-structure.html
     public class FileReader : IFileReader
     {
         public byte[] ReadFile(string filePath) => File.ReadAllBytes(filePath);
